8d031da3be0a508dbdeb750ca107bd5798f2d654
scain <scain>
2024-10-07 11:29:43 -0400

    Finished moving angular to nx

--- typescript/apps/angular/web-app-2/src/app/app.routes.ts
+++ typescript/apps/angular/web-app-2/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from '@feature/authentication';
 
 export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
