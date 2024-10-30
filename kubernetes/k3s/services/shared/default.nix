502d81a7fa3aa98ae3ed46afb1f84be15296d9ee
Ryan Housand <rhousand@gmail.com>
2024-10-30 14:57:05 -0400

    Testing k3s with TS

--- kubernetes/k3s/services/shared/default.nix
+++ kubernetes/k3s/services/shared/default.nix
@@ -25,16 +25,16 @@
 
   # Select internationalisation properties.
   #  i18n.defaultLocale = "en_US.UTF-8";
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LANGUAGE = "en_US.UTF-8";
-      LC_ALL = "en_US.UTF-8";
-      LC_CTYPE = "en_US.UTF-8";
-      LANG = "en_US.UTF-8";
-    };
-
-};
+  #  i18n = {
+  #    defaultLocale = "en_US.UTF-8";
+  #    extraLocaleSettings = {
+  #      LANGUAGE = "en_US.UTF-8";
+  #      LC_ALL = "en_US.UTF-8";
+  #      LC_CTYPE = "en_US.UTF-8";
+  #      LANG = "en_US.UTF-8";
+  #    };
+  #
+  #};
 
   console = {
     font = "Lat2-Terminus16";
