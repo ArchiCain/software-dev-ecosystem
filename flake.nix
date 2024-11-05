445e6d4b7bef55dd1ec22bb01ce10666476e4729
Ryan Housand <rhousand@gmail.com>
2024-11-05 16:17:40 -0500

    Add setup for localstack

--- flake.nix
+++ flake.nix
@@ -1,12 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-stable,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
@@ -14,18 +16,24 @@
         inherit system;
         config.allowUnfree = true;
       };
+      pkgsStable = import nixpkgs-stable {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in
       with pkgs; {
         devShells.default = mkShell {
           buildInputs = [
-            nodejs_20 # Install the latest version of NodeJs
-            yarn # Yarn for package management
             docker # Docker for containerization
+            go-task # Task for common commands
             kubectl # Kubernetes CLI tool
             kubernetes-helm # Helm, a Kubernetes package manager
+            liquibase # Add Liquibase for database migrations
+            localstack # Fully functional local Cloud stack
+            nodejs_20 # Install the latest version of NodeJs
             terraform # Infrustructure-as-Code (IaC) tool
-            go-task # Task for common commands
             tilt # Add Tilt for Kubernetes dev workflow
+            yarn # Yarn for package management
           ];
           shellHook = ''
             # Ensure Nx CLI is installed
@@ -34,6 +42,23 @@
               yarn global add nx
             else
               echo "Nx CLI is already installed."
+            fi
+
+            # Ensure Docker daemon is running
+            if ! command docker ps > /dev/null; then
+              echo "Docker daemon it not running; starting docker desktop"
+              open /Applications/Docker.app
+              sleep 15
+            else 
+              echo "Docker daemon is up and running"
+            fi
+
+            # Ensure localstack has been started
+            if ! command localstack status services > /dev/null; then
+              echo "Starting localstack"
+              localstack start -d
+            else
+              echo "localstack is up and running"
             fi
 
             # Add Yarn global bin to PATH (for globally installed packages like nx)
